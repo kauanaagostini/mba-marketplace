21016a9d102754e5507fa4af0f308dfb3ab8cb31
Kauana Maria Agostini <kauanaagostini@gmail.com>
2024-10-06 18:20:53 -0300

    feat: create auth layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom'
 
+import { Logo } from '@/assets/Logo'
+
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Cabeçalho Auth</h1>
-
+    <div className="min-h-screen grid grid-cols-2 overflow-hidden">
       <div>
+        <div className="ml-10 mt-10">
+          <Logo />
+        </div>
+        <div className="h-full bg-hero-auth-page bg-contain bg-no-repeat bg-top"></div>
+      </div>
+      <div className='overflow-auto'>
         <Outlet />
       </div>
     </div>
