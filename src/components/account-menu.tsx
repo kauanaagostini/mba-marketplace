f6265f77753af2e4854c36931b80402ced441246
Kauana Maria Agostini <kauanaagostini@gmail.com>
2024-10-13 13:31:28 -0300

    chore: adjusts in account menu, create get profile data, and sign out.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,10 @@
 import { DropdownMenuContent } from '@radix-ui/react-dropdown-menu'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
 
+import { getProfile } from '@/api/get-profile'
+import { signOut } from '@/api/sign-out'
 import { LogoutIcon } from '@/assets/icon/logout'
 import { UserAccountIcon } from '@/assets/icon/user-account'
 
@@ -13,6 +18,30 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  const navigate = useNavigate()
+  const [cookies, _, removeCookie] = useCookies(['auth'])
+
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+  })
+
+  async function handleSignOut() {
+    try {
+      await signOutFn()
+      if (cookies.auth) {
+        removeCookie('auth')
+      }
+      navigate('/sign-in', { replace: true })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -35,13 +64,22 @@ export function AccountMenu() {
         <DropdownMenuLabel className="flex items-center justify-around gap-2">
           <UserAccountIcon width={32} height={32} color="hsl(var(--primary))" />
           <h1 className="text-lg font-semibold text-secondary-foreground">
-            Seller
+            {profile?.seller.name}
           </h1>
         </DropdownMenuLabel>
         <DropdownMenuSeparator className="m-auto w-36 bg-accent" />
-        <DropdownMenuItem className="mx-3 flex cursor-pointer items-center justify-between fill-primary focus:bg-none">
-          <span className="text-sm font-semibold text-primary">Sair</span>
-          <LogoutIcon width={24} height={24} color="--primary" />
+        <DropdownMenuItem
+          asChild
+          className="mx-3 flex cursor-pointer items-center justify-between fill-primary focus:bg-none"
+          disabled={isSigningOut}
+        >
+          <button
+            className="flex w-40 items-center justify-between"
+            onClick={() => handleSignOut()}
+          >
+            <span className="text-sm font-semibold text-primary">Sair</span>
+            <LogoutIcon width={24} height={24} color="--primary" />
+          </button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
